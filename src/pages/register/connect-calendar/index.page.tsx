557e7cd0acaecdc5825d8d6f24d8d57b17ce6807
Matthew Araujo <matthewaraujo20@gmail.com>
2023-05-23 16:29:32 -0300

    modulos enviados

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,59 @@
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+import { Container, Header } from '../styles'
+import { ArrowRight, Check } from 'phosphor-react'
+import { AuthError, ConnectBox, ConnectIem } from './styles'
+import { signIn, useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+
+export default function Register() {
+  const session = useSession()
+  const router = useRouter()
+
+  const hasAuthError = Boolean(router.query.error)
+  const isSignedIn = session.status === 'authenticated'
+
+  async function handleConnectCalendar() {
+    await signIn('google')
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Conecte sua agenda!</Heading>
+        <Text>
+          Conecte o seu calendário para verificar automaticamente as horas
+          ocupadas e os novos eventos em que são agendados.
+        </Text>
+        <MultiStep size={4} currentStep={2}></MultiStep>
+      </Header>
+      <ConnectBox>
+        <ConnectIem>
+          <Text>Google Calendar</Text>
+          {isSignedIn ? (
+            <Button size="sm" disabled>
+              Conectado <Check />
+            </Button>
+          ) : (
+            <Button
+              variant="secondary"
+              size="sm"
+              onClick={handleConnectCalendar}
+            >
+              Conectar
+              <ArrowRight />
+            </Button>
+          )}
+        </ConnectIem>
+        {hasAuthError && (
+          <AuthError size="sm">
+            Falha ao se conectar ao Google, verifique se você habilitou as
+            permissões de acesso ao Google Calendar
+          </AuthError>
+        )}
+        <Button type="submit" disabled={!isSignedIn}>
+          Próximo passo <ArrowRight />
+        </Button>
+      </ConnectBox>
+    </Container>
+  )
+}
--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,21 +1,26 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
-import { Container, Form, FormError, Header } from './styles'
+import { AxiosError } from 'axios'
+import { useRouter } from 'next/router'
 import { ArrowRight } from 'phosphor-react'
+import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import { api } from '../../lib/axios'
+
+import { Container, Form, FormError, Header } from './styles'
 
 const registerFormSchema = z.object({
   username: z
     .string()
-    .min(3, { message: 'O nome de usuário deve ter no mínimo 3 caracteres' })
-    .regex(/^([a-z\\-])/i, {
-      message: 'O nome de usuário deve conter apenas letras e hífens',
+    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras.' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O usuário pode ter apenas letras e hifens.',
     })
     .transform((username) => username.toLowerCase()),
-  name: z.string().min(3, { message: 'O nome deve ter no mínimo 3 letras' }),
+  name: z
+    .string()
+    .min(3, { message: 'O nome precisa ter pelo menos 3 letras.' }),
 })
 
 type RegisterFormData = z.infer<typeof registerFormSchema>
@@ -31,44 +36,69 @@ export default function Register() {
   })
 
   const router = useRouter()
+
   useEffect(() => {
     if (router.query.username) {
       setValue('username', String(router.query.username))
     }
-  }, [router.query.username, setValue])
+  }, [router.query?.username, setValue])
+
   async function handleRegister(data: RegisterFormData) {
-    console.log(data)
+    try {
+      await api.post('/users', {
+        name: data.name,
+        username: data.username,
+      })
+
+      await router.push('/register/connect-calendar')
+    } catch (err) {
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        alert(err.response.data.message)
+        return
+      }
+
+      console.error(err)
+    }
   }
 
   return (
     <Container>
       <Header>
-        <Heading as="strong">Bem vindo ao Ignite Call!</Heading>
+        <Heading as="strong">Bem-vindo ao Ignite Call!</Heading>
         <Text>
           Precisamos de algumas informações para criar seu perfil! Ah, você pode
-          editar essas informações depois
+          editar essas informações depois.
         </Text>
-        <MultiStep size={4} currentStep={1}></MultiStep>
+
+        <MultiStep size={4} currentStep={1} />
       </Header>
+
       <Form as="form" onSubmit={handleSubmit(handleRegister)}>
         <label>
-          <Text size="sm">Nome do usuario</Text>
+          <Text size="sm">Nome de usuário</Text>
           <TextInput
             prefix="ignite.com/"
-            placeholder="seu-usuario"
+            placeholder="seu-usuário"
             {...register('username')}
           />
-          {errors.username && <FormError>{errors.username.message}</FormError>}
+
+          {errors.username && (
+            <FormError size="sm">{errors.username.message}</FormError>
+          )}
         </label>
 
         <label>
           <Text size="sm">Nome completo</Text>
           <TextInput placeholder="Seu nome" {...register('name')} />
-          {errors.name && <FormError>{errors.name.message}</FormError>}
+
+          {errors.name && (
+            <FormError size="sm">{errors.name.message}</FormError>
+          )}
         </label>
 
         <Button type="submit" disabled={isSubmitting}>
-          Próximo passo <ArrowRight />
+          Próximo passo
+          <ArrowRight />
         </Button>
       </Form>
     </Container>
